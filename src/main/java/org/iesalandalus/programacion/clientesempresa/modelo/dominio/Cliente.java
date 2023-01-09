8f4d8dcd4432ea7c76016d941680a58317f42850
JCarlosOrtega <j.karlox@gmail.com>
2023-01-09 23:10:31 +0100

    Creados atributos con visibilidad adecuada

--- src/main/java/org/iesalandalus/programacion/clientesempresa/modelo/dominio/Cliente.java
+++ src/main/java/org/iesalandalus/programacion/clientesempresa/modelo/dominio/Cliente.java
@@ -1,5 +1,13 @@
 package org.iesalandalus.programacion.clientesempresa.modelo.dominio;
 
+import java.time.LocalDate;
+
 public class Cliente {
 
+	private String ER_Correo, ER_DNI, ER_TELEFONO;
+	public String FORMATO_FECHA;
+	private String nombre, dni, correo, telefono;
+	private LocalDate fechaNacimiento;
+	
+	
 }
